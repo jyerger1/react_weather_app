99ee9979cf26226dec4f5bd81556c777d61c7e47
John Yerger <johnyerger@johns-mbp.home>
2020-04-24 16:45:59 -0400

    weather and time working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
-// import "moment";
-// import tz from "zipcode-to-timezone";
+import Moment from "moment";
+import tz from "zipcode-to-timezone";
+import "moment-timezone";
 import "./App.css";
 
 // const API_Key = "98714695d6d6eca2461e95c2aa78b140";
 
 class App extends Component {
-  state = {
-    // temperature: undefined,
-    // city: undefined,
-    // country: undefined,
-    // humidity: undefined,
-    // description: undefined,
-    // error: undefined,
+  state = {};
+
+  getTime = () => {
+    const zone = tz.lookup(this.state.zip);
+    const now = Moment().tz(zone).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    console.log(now);
+    this.setState({
+      time: now,
+    });
   };
-
-  // getTime = () => {
-
-  // };
 
   getWeather = async () => {
     const inputValue = document.getElementById("zipInput").value;
@@ -33,15 +32,20 @@ class App extends Component {
     console.log(data);
 
     this.setState({
+      name: data.name,
       temperature: data.main.temp,
+      description: data.weather[0].description,
+      zip: inputValue,
     });
+
+    this.getTime();
   };
 
   // RENDER TO THE PAGE
   render() {
     return (
       <div id="form">
-        <h1>Get Weather</h1>
+        <h1>GET WEATHER</h1>
         <div>
           <p>
             <input
@@ -54,7 +58,10 @@ class App extends Component {
           <p>
             <button onClick={this.getWeather}>Click</button>
           </p>
+          <p>{this.state.name}</p>
           <p>{this.state.temperature}</p>
+          <p>{this.state.description}</p>
+          <p>{this.state.time}</p>
         </div>
       </div>
     );
